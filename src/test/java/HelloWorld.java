77bb392bf80f186e083bf7aa22dc60395a78ecb0
Yevhenii_Ivanenko <Yevhenii_Ivanenko@epam.com>
2023-06-08 10:12:09 +0300

    FirestAttempt

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://selenium.dev");
+        WebElement SupportButton =  driver.findElement(By.xpath("//*[@id='main_navbar']/ul/li[6]/a"));
+        SupportButton.click();
+        Thread.sleep(5000); //do not use this approach
+        driver.quit();
+    }
+
+}
