6d5aa6055c03a7cc1a44571e62562ace0472aabe
Yevhenii_Ivanenko <Yevhenii_Ivanenko@epam.com>
2023-06-09 09:50:06 +0300

    TestNGlearning

--- src/test/java/TestNG/TestNGTest.java
+++ src/test/java/TestNG/TestNGTest.java
@@ -17,7 +17,9 @@ public class TestNGTest {
         driver.get("https://selenium.dev");
         WebElement SupportButton =  driver.findElement(By.xpath("//*[@id='main_navbar']/ul/li[6]/a"));
         SupportButton.click();
-        Assert.assertTrue(SupportButton.isSelected());//change to something new
+//        Assert.assertTrue(SupportButton.isSelected());
+        WebElement SupportTitle = driver.findElement(By.xpath("//h1[text()='Getting Help']"));
+        Assert.assertEquals(SupportTitle.getText(),"Getting Help");
         //Thread.sleep(3000); //do not use this approach
         driver.quit();
     }
