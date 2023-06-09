f5b7473d4c63ced32e3433c294fed53380b7c62b
Yevhenii_Ivanenko <Yevhenii_Ivanenko@epam.com>
2023-06-09 11:19:28 +0300

    added Before/After methods and small improvements

--- src/test/java/TestNG/TestNGTest.java
+++ src/test/java/TestNG/TestNGTest.java
@@ -5,23 +5,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 //import java.lang.annotation.Annotation;
 
 public class TestNGTest {
-    @Test
+    private WebDriver driver;
+    @BeforeMethod (alwaysRun = true)
+    public void browserSetup() {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+    }
+    @Test (description = "elementary test, CTRA-7465")
     public void BtnClick()  {
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.get("https://selenium.dev");
         WebElement SupportButton =  driver.findElement(By.xpath("//*[@id='main_navbar']/ul/li[6]/a"));
         SupportButton.click();
-//        Assert.assertTrue(SupportButton.isSelected());
+//      Assert.assertTrue(SupportButton.isSelected());
         WebElement SupportTitle = driver.findElement(By.xpath("//h1[text()='Getting Help']"));
         Assert.assertEquals(SupportTitle.getText(),"Getting Help");
-        //Thread.sleep(3000); //do not use this approach
+//      Thread.sleep(3000); //do not use this approach
         driver.quit();
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void quitBrowser(){
+        driver.quit();
+        driver = null; // to get clear instance of browser for other tests
     }
 
 }
