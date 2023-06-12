c6558118d84a1058b67fab6edd1940c7814e3ded
Yevhenii_Ivanenko <Yevhenii_Ivanenko@epam.com>
2023-06-12 12:27:36 +0300

    removed some comments

--- src/test/java/pageobject_model/SeleniumSupportPage.java
+++ src/test/java/pageobject_model/SeleniumSupportPage.java
@@ -5,11 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class SeleniumSupportPage {
-    //private WebDriver driver;
     private static final String HOMEPAGE_URL = "https://selenium.dev";
-
-    //@FindBy(xpath = "//a[@href='/support']")
-    /*private WebElement ;*/
 
     public void openSupportPage(WebDriver driver) {
         driver.get(HOMEPAGE_URL);
