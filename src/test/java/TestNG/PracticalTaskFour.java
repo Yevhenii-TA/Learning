c475823a46c0b2c65aa0f0a745650c98edc8d989
Yevhenii_Ivanenko <Yevhenii_Ivanenko@epam.com>
2023-06-19 17:37:11 +0300

    some preparations for task #4

---
+++ src/test/java/TestNG/PracticalTaskFour.java
@@ -0,0 +1,64 @@
+package TestNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageobject_model.PractTaskFourPOM;
+
+public class PracticalTaskFour {
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void BrowserSetup() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL); //waits for all resources to download
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TaskFour() throws InterruptedException {
+        String epectedPrice = "Total Estimated Cost: USD 4,799.62 per 1 month";
+        String expectedSSD = "Local SSD: 2x375 GiB";
+        String expectedVM = "Provisioning model: Regular";
+        String expectedInstance = "Instance type: n1-standard-1";
+        String expectedRegion = "Region: Columbus";
+
+        PractTaskFourPOM practTaskPOM = new PractTaskFourPOM(driver);
+        practTaskPOM.openHomePage()
+                .searchCalcAndNavigate()
+                .fillInMainData()
+                .fillInGPUdata()
+                .fillInStorageData()
+                .submitForm();
+
+        WebElement actualPrice = driver.findElement(By.xpath("//*[@id='resultBlock']/md-card/md-card-content/div/div/div/div[1]/h2/b"));
+        WebElement actualSSD = driver.findElement(By.xpath("//*[@id='compute']/md-list/md-list-item[8]/div[1]")); //*[@id="compute"]/md-list/md-list-item[8]/div[1]/text()
+        WebElement actualVM = driver.findElement(By.xpath("//*[@id='compute']/md-list/md-list-item[4]/div[1]"));
+        WebElement actualInstance = driver.findElement(By.xpath("//*[@id='compute']/md-list/md-list-item[5]/div[1]"));
+        WebElement actualRegion = driver.findElement(By.xpath("//*[@id='compute']/md-list/md-list-item[1]/div[1]"));
+
+        Assert.assertEquals(actualPrice.getText(), epectedPrice);
+        if (actualSSD.getText().contains(expectedSSD)){
+            Assert.assertTrue(true,"String is not valid");
+        }
+        Assert.assertEquals(actualVM.getText(), expectedVM);
+        if (actualInstance.getText().contains(expectedInstance)){
+            Assert.assertTrue(true,"String is not valid");
+        }
+        Assert.assertEquals(actualRegion.getText(), expectedRegion);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void TearDown() {
+        driver.quit();
+        driver = null;
+    }
+}
---
+++ src/test/java/pageobject_model/PractTaskFourPOM.java
@@ -0,0 +1,157 @@
+package pageobject_model;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PractTaskFourPOM {
+    private final WebDriver driver;
+    private static final String HOMEPAGE_URL = "https://cloud.google.com/";
+    private static final String CALC_NAME = "Google Cloud Platform Pricing Calculator";
+
+
+    //region Selectors
+    @FindBy(xpath = "//*[@aria-label='Search']")
+    private WebElement searchBtnField;
+    @FindBy(xpath = "//*[@id='___gcse_0']/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a")
+    private WebElement calculatorLink;
+    @FindBy(xpath = "//md-tab-item[@tabindex='0']")
+    private WebElement computeEngineBtn;
+    @FindBy(xpath = "//*[@id='input_96']")
+    private WebElement numberOfInstances; //#input_96
+    @FindBy(xpath = "//*[contains(text(),'E2')]/../../../..//md-select[@placeholder='Series']")
+    private WebElement machineSeries;
+    @FindBy(xpath = "//md-option[@id='select_option_212']")
+    private WebElement machineSeriesOption;
+    @FindBy(xpath = "//md-option[@value='CP-COMPUTEENGINE-VMIMAGE-N1-STANDARD-8']")
+    private WebElement machineTypeOption;
+    //@FindBy(xpath = "//md-chekbox[@aria-label='Add GPUs']")
+    @FindBy(xpath = "//*[@id='mainForm']/div[2]/div/md-card/md-card-content/div/div[1]/form/div[12]/div[1]/md-input-container/md-checkbox")
+    // ask about this
+    private WebElement checkboxGPU;
+    @FindBy(xpath = "//*[@id='select_484']")
+    private WebElement typeGPU;
+    @FindBy(xpath = "//*[@id='select_option_491']")//*[@id="select_option_491"]
+    private WebElement typeGPUSelect;
+    @FindBy(xpath = "//*[@id='select_value_label_483']/span[1]/div")
+    private WebElement numberOfGPU;
+    @FindBy(xpath = "//*[@id='select_option_494']") //*[@id="select_option_494"]    //*[@id="select_option_494"]/div
+    private WebElement numberOfGPUSelect;
+    @FindBy(xpath = "//*[@id='select_value_label_446']")
+    private WebElement localSSD;
+    @FindBy(xpath = "//*[@id='select_option_473']")
+    private WebElement localSSDSelect;
+    @FindBy(css = "#select_value_label_94")
+    private WebElement dataCenter;
+    @FindBy(css = "#select_option_232") //changed data center because the asked one is not supported with selected GPU
+    private WebElement dataCenterSelect;
+    @FindBy(css = "#select_value_label_95")
+    private WebElement commitedUsage;
+    @FindBy(css = "#select_option_134")
+    private WebElement commitedUsageSelect;
+    @FindBy(xpath = "//*[@id='mainForm']/div[2]/div/md-card/md-card-content/div/div[1]/form/div[19]/button")
+    private WebElement addToEstimate;
+    @FindBy(xpath = "//iframe[@src='https://cloud.google.com/frame/products/calculator/index_d6a98ba38837346d20babc06ff2153b68c2990fa24322fe52c5f83ec3a78c6a0.frame']")
+    private WebElement calculatorFrameOne;
+    @FindBy(xpath = "//iframe[@id='myFrame']")
+    private WebElement calculatorFrameTwo;
+    @FindBy(xpath = "//*[@id='Email Estimate']")
+    private WebElement openEmailForm;
+    @FindBy(xpath = "//*[@id='input_566']")
+    private WebElement emailField;
+    @FindBy(xpath = "")
+    private WebElement sendEmailBtn;
+
+    //endregion
+    public PractTaskFourPOM(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public PractTaskFourPOM openHomePage() {
+        driver.get(HOMEPAGE_URL);
+        return this;
+    }
+
+    public PractTaskFourPOM searchCalcAndNavigate() {
+        searchBtnField.sendKeys(CALC_NAME);
+        searchBtnField.sendKeys(Keys.ENTER);
+        waitHandlerVisibility(5, calculatorLink);
+        calculatorLink.click();
+        return this;
+    }
+
+    public PractTaskFourPOM fillInMainData() throws InterruptedException {
+        switchToFrame(); //switch to iframes
+        numberOfInstances.sendKeys("4");
+        machineSeries.click();
+        waitHandlerVisibility(1, machineSeriesOption);
+        machineSeriesOption.click();
+        return this;
+    }
+
+    public PractTaskFourPOM fillInGPUdata() {
+        checkboxGPU.click();
+        typeGPU.click();
+        waitHandlerVisibility(2, typeGPUSelect);
+        typeGPUSelect.click();
+        numberOfGPU.click();
+        waitHandlerVisibility(2, numberOfGPUSelect);
+        numberOfGPUSelect.click();
+        return this;
+    }
+
+    public PractTaskFourPOM fillInStorageData() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        localSSD.click();
+        waitHandlerVisibility(2, localSSDSelect);
+        localSSDSelect.click();
+
+        WebElement point = driver.findElement(By.xpath("//*[@id='mainForm']/div[2]/div/md-card/md-card-content/div/div[1]/form/div[17]/div[1]"));
+        js.executeScript("arguments[0].scrollIntoView();", point);
+        js.executeScript("arguments[0].click();", dataCenter);
+        js.executeScript("arguments[0].click();", dataCenterSelect);
+        js.executeScript("arguments[0].click();", commitedUsage);
+        waitHandlerClickable(3, commitedUsageSelect);
+        js.executeScript("arguments[0].click();", commitedUsageSelect);
+
+        return this;
+    }
+
+    public PractTaskFourPOM waitHandlerVisibility(long secDuration, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(secDuration));
+        wait.until(ExpectedConditions.visibilityOf(element));
+        return this;
+    }
+
+    public PractTaskFourPOM waitHandlerClickable(long secDuration, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(secDuration));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        return this;
+    }
+
+    public PractTaskFourPOM switchToFrame() {
+        driver.switchTo().frame(calculatorFrameOne);
+        driver.switchTo().frame(calculatorFrameTwo);
+        return this;
+    }
+
+    public PractTaskFourPOM submitForm() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", addToEstimate);
+        //addToEstimate.click();
+        return this;
+    }
+
+    public PractTaskFourPOM sendByEmail() {
+
+        openEmailForm.click();
+        emailField.sendKeys();
+        return this;
+    }
+}
